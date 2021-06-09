3ee5cde6dd4092d1a291fb38871452aa75ad48c1
ivan <spishniak@gmail.com>
2021-06-09 20:09:29 +0300

    all tasks version 1

--- week0/string.js
+++ week0/string.js
@@ -45,20 +45,40 @@ console.log('5:', replaced.slice(3,7) );
 
 // 6. Divide replaced string by number 10
 
-console.log('6:', )
+console.log('6:', replaced.split('10').join('')) // not understand task
 
 // 7. Write function capitalize(), to capitalize first letter of string 
 const lowerCased = 'new york';
-
+function capitalize (lowerCased){
+    result = ''
+  return  result = lowerCased[0].toUpperCase() + lowerCased.slice(1)
+}
 console.log('7:', capitalize(lowerCased));
 
 // 8. Write insert function, that will accept main string, string to insert and position to insert
 // Change **** with your name 
 
 const mainString = 'Hello, my name is ****, I want to learn Node.js';
-console.log('8:' , insert(mainString, fullName, *****))
+// console.log('8:' , insert(mainString, fullName, '*****')) // don't understand task
+
+console.log(`Hello, my name is ${fullName}, I want to learn Node.js`)
+
+function insert(mainString){
+   return mainString.replace(/\*+/, fullName);
+    
+}
+console.log(insert(mainString))
 
 // 9. Write your own reverse() function. (do not use .reverse())
+
+function reverse(str){
+    let result = ''
+    for(let i = str.length -1; i >= 0; i--){
+        result += str[i];
+    }
+    return result
+}
+
 
 console.log('9', reverse('my random string'));
 
